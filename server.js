87c1af1d44c2c4996550c8cc0c64bed7f9580d38
Donago159 <155327436+Donago159@users.noreply.github.com>
2025-07-27 15:19:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/brawl/*', async (req, res) => {
         });
         res.json(response.data);
     } catch (error) {
-        res.status(error.response?.status || 500).json({ error: error.response?.data || error.message });
+        res.status(error.response?.status || 500).json({req});
     }
 });
 
